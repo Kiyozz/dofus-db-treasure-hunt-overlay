111b4fe6efd2910e85f2a9757698cd9a1d6e7836
Kévin TURMEL <kevinturmel@gmail.com>
2021-10-07 09:50:43 +0200

    Update dependencies

---
+++ esbuild.main.config.ts
@@ -0,0 +1,11 @@
+import { BuildOptions } from 'esbuild'
+import * as path from 'path'
+
+const config: BuildOptions = {
+  platform: 'node',
+  entryPoints: [path.resolve('src/main/main.ts'), path.resolve('src/main/preload.ts')],
+  bundle: true,
+  target: 'node16.5.0', // electron version target
+}
+
+export default config
