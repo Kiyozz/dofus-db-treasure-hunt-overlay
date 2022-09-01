c81c6e04ec0d9c1b1adf27dd38541d6f84b63a89
Skijearz <doerrie.dominic@web.de>
2022-09-01 05:24:30 +0200

    Revert "fixed the weird behaviour of the elements inside the dropdown menu"
    This reverts commit db2c2c682e0d061eebf35e47948f5370d2a8a60a.

--- preload.ts
+++
@@ -1,7 +0,0 @@
-import { ipcRenderer, contextBridge } from 'electron'
-
-contextBridge.exposeInMainWorld('dmo', {
-  quit: () => ipcRenderer.invoke('quit'),
-  goToWebsite: () => ipcRenderer.invoke('go-to-website'),
-  changeLanguage:(data : string) => ipcRenderer.invoke('changeLanguage',data),
-})
